84fbb4761cf8e87dac5bf6a807c539ae433d0f7d
alex94040 <alex94040@gmail.com>
2009-05-30 00:32:19 +0000

    Initial checkin of the real QFirebug control

---
+++ QFirebug/source/install.php
@@ -0,0 +1,29 @@
+<?php
+
+$objPlugin = new QPlugin();
+$objPlugin->strName = "QFirebug";
+$objPlugin->strDescription = 'Plugin that eases debugging of QCubed applications through ' .
+			'integration with Firebug, an excellent Firefox extension for Javascript debugging.';
+$objPlugin->strVersion = "0.1";
+$objPlugin->strPlatformVersion = "1.1";
+$objPlugin->strAuthorName = "Eduardo Garcia aka enzo";
+$objPlugin->strAuthorEmail ="enzo [at] anexusit [dot] com";
+
+$components = array();
+
+$components[] = new QPluginMiscIncludedFile("includes/QFirebug.class.php");
+$components[] = new QPluginMiscIncludedFile("includes/FirePHP.class.php");
+$components[] = new QPluginMiscIncludedFile("includes/fb.php");
+
+$components[] = new QPluginExampleFile("example/qfirebug.php");
+$components[] = new QPluginExampleFile("example/qfirebug.tpl.php");
+$components[] = new QPluginExampleFile("example/qfirebug.png");
+
+$components[] = new QPluginIncludedClass("QFirebug", "QFirebug.class.php");
+
+$components[] = new QPluginExample("example/qfirebug.php", "Introduction to Debugging with QFirebug");
+
+$objPlugin->addComponents($components);
+$objPlugin->install();
+	
+?>
