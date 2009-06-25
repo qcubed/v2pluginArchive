a87110c31f3752afd693c3873e7b64f64a69a675
alex94040 <alex94040@gmail.com>
2009-06-25 22:29:05 +0000

    Fixing a grammatical mistake.

--- QObjectDiff/source/install.php
+++ QObjectDiff/source/install.php
@@ -2,8 +2,8 @@
 
 $objPlugin = new QPlugin();
 $objPlugin->strName = "QObjectDiff";
-$objPlugin->strDescription = 'This plugin lets you determine which fields have changed '.
-	'in the QForm since the user last saw it: which fields did they edited.';
+$objPlugin->strDescription = 'Determine which fields have changed '.
+	'in the QForm since the user last saw it: which fields did they edit.';
 $objPlugin->strVersion = "0.1";
 $objPlugin->strPlatformVersion = "1.1";
 $objPlugin->strAuthorName = "Alex Weinstein, a.k.a. alex94040";
