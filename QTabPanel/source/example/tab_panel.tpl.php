4a5d70c7c09534b408ae52e661517fb219a60898
alex94040 <alex94040@gmail.com>
2009-09-24 21:35:05 +0000

    QTabPanel plugin: light code cleanup, as well as improvements for the example text.

--- QTabPanel/source/example/tab_panel.tpl.php
+++ QTabPanel/source/example/tab_panel.tpl.php
@@ -2,20 +2,42 @@
 	<?php $this->RenderBegin(); ?>
 
 	<div class="instructions">
-		<div class="instruction_title">Tab Panels</div>
-
-		This is just a simple example to show how we could create in an easy way tab to wrap 
-		our control in order to provide a fancy interface to our user.
+		<div class="instruction_title">Tabular Interfaces using QTabPanel</div>
+		<b>QTabPanel</b> offers a way to wrap a tabular interface, similar to what you
+		frequently see in rich-client applications. <br /><br />
 		
-		Tab panel enable to crea tabs inside other tabs as you can see in the tab # one.
+		Using it is really simple: create a container <b>QTabPanel</b> control. Then,
+		create <b>QTabPanelSections</b>, one for each tab, specifying the <b>QTabPanel</b> as
+		their parent. Remember how you have to pass the parent in the constructor of any
+		<b>QControl</b>? Here, you pass in the <b>QTabPanel</b>, not the <b>$this</b>:<br />
 		
-		We create our tabs hierarchy on our form , but in our template we just need render the main parent tab. 
+		<div style="padding-left: 50px">
+			<code>
+				$this->myTabPanel = new QTabPanel($this);<br />
+				$sectionA = new QTabPanelSection($this->myTabPanel);<br />
+				$sectionB = new QTabPanelSection($this->myTabPanel);
+			</code>
+		</div>
+		<br>
+			
+		Then, just add your QControls to the right sections - the way to do it is again
+		by specifying the section as the parent parameter in the QControl constructor. So
+		really, what you're doing is specifying a hierarchy of visual elements. The root of the
+		hierarchy is the parent QForm; it hosts a tab panel; the tab panel hosts the sections; each of the sections
+		hosts one or more controls.<br /><br />
 		
-		Note that this control use css, so we could change the look and fell accordingly our needs.
+		You may also host <b>QTabPanelSection</b>'s inside other <b>QTabPanelSection</b>,
+		thus creating a tabbed control inside a tab. This functionality is currently
+		experimental. <br /><br />
+		
+		Note that this control uses CSS extensively, so it's really easy , so we could change the look and fell accordingly our needs.
 	</div>
 	<div id="tabpanel" style="clear: both">
 	<?php 
 		$this->tabPanel->Render();
+	?>
+		<br /><br />		
+	<?php
 		$this->btnSave->Render();
 		$this->btnCancel->Render();
 	?>
