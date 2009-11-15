6ce67ac5525299116a7cf8bc2b5156a507849a9e
alex94040 <alex94040@gmail.com>
2009-11-15 18:22:29 +0000

    Reworked examples for the QFlot plugin.

--- QFlot/source/install.php
+++ QFlot/source/install.php
@@ -2,8 +2,8 @@
 
 $objPlugin = new QPlugin();
 $objPlugin->strName = "QFlot";
-$objPlugin->strDescription = 'Use JS flot library to create charts in QCodo using client side';
-$objPlugin->strVersion = "0.1";
+$objPlugin->strDescription = 'Use a JavaScript-based plotting library called Flot to create nice-looking charts. Based on a jQuery.';
+$objPlugin->strVersion = "0.2";
 $objPlugin->strPlatformVersion = "1.1";
 $objPlugin->strAuthorName = "Eduardo Garcia aka enzo";
 $objPlugin->strAuthorEmail ="enzo [at] anexusit [dot] com";
@@ -13,7 +13,7 @@ $components = array();
 $components[] = new QPluginControlFile("includes/QFlot.class.php");
 $components[] = new QPluginControlFile("includes/QFlotSeries.class.php");
 
-$components[] = new QPluginIncludedClass("QFlot", 		"includes/QFlot.class.php");
+$components[] = new QPluginIncludedClass("QFlot", "includes/QFlot.class.php");
 $components[] = new QPluginIncludedClass("QFlotSeries", "includes/QFlotSeries.class.php");
 
 $components[] = new QPluginJsFile("js/jquery.flot.pack.js");
@@ -24,8 +24,8 @@ $components[] = new QPluginExampleFile("example/qflot-bar.tpl.php");
 $components[] = new QPluginExampleFile("example/qflot-timeseries.php");
 $components[] = new QPluginExampleFile("example/qflot-timeseries.tpl.php");
 
-$components[] = new QPluginExample("example/qflot-bar.php", "QFlot: Generate a bar chart using JS");
-$components[] = new QPluginExample("example/qflot-timeseries.php", "QFlot Series: Generate a time series chart using JS");
+$components[] = new QPluginExample("example/qflot-bar.php", "QFlot: Display a bar chart");
+$components[] = new QPluginExample("example/qflot-timeseries.php", "QFlot: Display a time series chart");
 
 $objPlugin->addComponents($components);
 $objPlugin->install();
