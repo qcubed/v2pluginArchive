0d7935bde4ff265a9162dc051bdc49fb84e7d9d0
Vardan Akopian <vakopian+github@gmail.com>
2010-05-26 22:32:00 +0000

    range conditions for QQ

---
+++ QQConditionRange/source/install.php
@@ -0,0 +1,23 @@
+<?php
+
+$objPlugin = new QPlugin();
+$objPlugin->strName = "QQConditionRange";
+$objPlugin->strDescription = 'Range query for QQuery.';
+$objPlugin->strVersion = "0.1";
+$objPlugin->strPlatformVersion = "2.0";
+$objPlugin->strAuthorName = "Vardan Akopian";
+$objPlugin->strAuthorEmail = "vakopian+qcubed [at] gmail [dot] com";
+
+$components = array();
+
+$components[] = new QPluginControlFile("includes/QQConditionRange.class.php");
+$components[] = new QPluginIncludedClass("QQConditionRange", "includes/QQConditionRange.class.php");
+
+$components[] = new QPluginExample("example/conditionrange.php", "QQConditionRange: Range query for QQuery");
+$components[] = new QPluginExampleFile("example/conditionrange.php");
+$components[] = new QPluginExampleFile("example/conditionrange.tpl.php");
+
+$objPlugin->addComponents($components);
+$objPlugin->install();
+
+?>
